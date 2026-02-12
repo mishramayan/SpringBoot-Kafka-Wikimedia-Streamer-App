8c137eb3644621a61a405485a4461092797b757d
Mayan Kumar Mishra <91338576+mishramayan@users.noreply.github.com>
2026-02-12 13:28:05 -0600

    Created project boiler plate and included a sub-module for kafka-producer

---
+++ src/main/java/net/java/springboot/SpringBootKafkaWikimediaStreamerAppApplication.java
@@ -0,0 +1,13 @@
+package net.java.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaWikimediaStreamerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaWikimediaStreamerAppApplication.class, args);
+	}
+
+}
